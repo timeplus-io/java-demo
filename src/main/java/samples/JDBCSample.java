a9c2fc8a4288b46054cf6e12b2ce8b1cc834043f
Gang Tao <gang@timeplus.io>
2022-10-18 17:01:08 -0700

    add a minimal query sample

--- src/main/java/samples/JDBCSample.java
+++ src/main/java/samples/JDBCSample.java
@@ -7,8 +7,15 @@ public class JDBCSample {
         try {
             Class.forName("timeplus.io.jdbc.TimeplusDriver");
             final String jdbcUrl = "jdbc:TimeplusDriver:test";
-            final Connection con = DriverManager.getConnection(jdbcUrl);
+            final Connection conn = DriverManager.getConnection(jdbcUrl);
             System.out.println("Connected to Timeplus!");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT time, speed_kmh, cid FROM car_live_data");
+            while (rs.next()) {
+                String cid = rs.getString("cid");
+                System.out.println(cid + "\n");
+            }
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
